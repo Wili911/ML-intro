78902a6867db5bf2d76cb4c36d9ed0357f959439
wili911 <guillem911@gmail.com>
2023-10-08 17:46:34 +0200

    Change folder

---
+++ SVM/plotSVMgoodHyperplane.m
@@ -0,0 +1,43 @@
+function [] =  plotSVMgoodHyperplane(points, w, b)
+    if isempty(points)
+        print("La llista de punts introduïda és buida.")
+        return
+    end
+
+    % Classifica els punts
+    [~, class1, class2] = classifyPoints(w, b, points);
+
+    % Plot the points with different colors for each class
+    figure;
+    if ~isempty(class1)
+        scatter(class1(:, 1), class1(:, 2), 'r', 'filled', 'DisplayName', 'Classe 1');
+    end
+        hold on;
+    if ~isempty(class2)
+        scatter(class2(:, 1), class2(:, 2), 'b', 'filled', 'DisplayName', 'Classe 2');
+    end
+    % Plot the hyperplane as a line
+    xRange = [-0.1,1.1];
+    x = (xRange(1)-1):0.1:(xRange(2)+1);
+    y = (-w(1) * x - b) / w(2);
+    plot(x, y, 'k', 'LineWidth', 1.5, 'DisplayName', 'Hiperpla');
+    
+    [~,i] = min(abs((w*class1' + b)/norm(w)));
+    c = -class1(i,2)*w(2)-class1(i,1)*w(1);
+    y = (-w(1) * x - c) / w(2);
+    plot(x, y, 'k--', 'LineWidth', 1.5, 'DisplayName', 'Hiperpla');
+    
+    [~,i] = min(abs((w*class2' + b)/norm(w)));
+    c = -class2(i,2)*w(2)-class2(i,1)*w(1);
+    y = (-w(1) * x - c) / w(2);
+    plot(x, y, 'k--', 'LineWidth', 1.5, 'DisplayName', 'Hiperpla');
+    
+
+    xlim(xRange)
+    ylim([-0.1,1.1])
+    grid off;
+    set(gca, 'XTickLabel', []);
+    set(gca, 'YTickLabel', []);
+    set(gca, 'XTick', []);
+    set(gca, 'YTick', []);
+    hold off;
---
+++ SVM/trainSoftSVM.m
@@ -0,0 +1,17 @@
+function [w, b, xi] = trainSoftSVM(points, points_classes, C)
+% points n x 2
+% points_classes n x 1
+    [n, d] = size(points);
+    H = zeros(n+d+1);
+    H(1:d,1:d) = ones(d);
+    f = [zeros(1,d), C*ones(1,n), 0];
+    bpoints = points_classes .* [points ones(n,1)];
+    bpoints = [bpoints eye(n)];
+    B = -ones(n,1);
+    A = -bpoints;
+    lb = [-Inf * ones(1,d+1) zeros(1,n)];
+    options = optimoptions('quadprog','Display','off');
+    x = quadprog(H,f,A,B,[],[],lb,[],[],options);
+    w = x(1:d)';
+    b = x(d+1);
+    xi = x(d+2:end)';
